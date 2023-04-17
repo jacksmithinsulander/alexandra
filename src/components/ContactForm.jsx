084293f80a087cc2bd0c158382d15827b2b744a7
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-04-17 12:16:41 +0200

    form extended

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,6 +3,16 @@ import { ContactField } from "./../models/ContactField";
 
 export const ContactForm =  ({ contactMe }) => {
 
-	const [form, submitForm] = useState(new ContactField());
+	const [form, submitForm] = useState(new ContactField("", "", ""));
 
+	const handleChange = (e) => {
+		if (e.target.typ === "text") {
+			submitForm({...form, })
+		}
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		cpmtactMe(form);
+	};
 }
